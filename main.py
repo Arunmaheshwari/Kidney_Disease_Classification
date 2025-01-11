d834e12fb7d111df8bf074a0c3a050c588cbdf48
Arun Maheshwari <maheshwariarun940@gmail.com>
2025-01-11 15:52:32 -0500

    Created files for Model training

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
+from src.Kidney_Disease_Classifier.pipeline.stage_01_data_ingestion import DataIngestionTrainingPipeline
+from src.Kidney_Disease_Classifier.pipeline.stage_03_model_training import ModelTrainingPipeline
 from src.Kidney_Disease_Classifier.pipeline.stage_02_prepare_base_model import PrepareBaseModelTrainingPipeline
 from src.Kidney_Disease_Classifier import logger
-from src.Kidney_Disease_Classifier.pipeline.stage_01_data_ingestion import DataIngestionTrainingPipeline
+
 
 
 
@@ -23,6 +25,20 @@ if __name__ == "__main__":
     try:
         logger.info(f">>>>>>> stage {STAGE_NAME} started <<<<<<<<<<")
         obj = PrepareBaseModelTrainingPipeline()
+        obj.main()
+        logger.info(f">>>>>>>>>> stage {STAGE_NAME} completed <<<<<<<<\n\nx=============x")
+    except Exception as e:
+        logger.exception(e)
+        raise e
+    
+
+STAGE_NAME = "Training Model"
+
+
+if __name__ == "__main__":
+    try:
+        logger.info(f">>>>>>> stage {STAGE_NAME} started <<<<<<<<<<")
+        obj = ModelTrainingPipeline()
         obj.main()
         logger.info(f">>>>>>>>>> stage {STAGE_NAME} completed <<<<<<<<\n\nx=============x")
     except Exception as e:
